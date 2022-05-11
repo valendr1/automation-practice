6acc2b41fb7c88d71fecc496c26051c42da89428
Valery <valerijvaler931@gmail.com>
2022-05-11 14:03:41 +0500

    added owner

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,8 +2,10 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,16 +14,27 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+    static String login = config.login();
+    static String password = config.password();
+    static String selenoidSign = "https://" + login + ":" + password + "@selenoid.autotests.cloud/wd/hub";
+
     @BeforeAll
     static void startup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = selenoidSign;
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "100");
+        String browserSize = System.getProperty("browserSize", "1920x1080");
+
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browserSize);
     }
 
     @AfterEach
